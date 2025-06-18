d717c6f29300db3d41f6c0ce40056960e2c4b768
Liang <chenliang.info@gmail.com>
2025-06-17 19:45:22 -0700

    cosmic

--- src/consts.ts
+++ src/consts.ts
@@ -29,16 +29,16 @@ export const PROJECTS: Metadata = {
 };
 
 export const SOCIALS: Socials = [
-  { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+  // { 
+  //   NAME: "twitter-x",
+  //   HREF: "https://twitter.com/markhorn_dev",
+  // },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/newcl"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/angelleecash",
   }
 ];
